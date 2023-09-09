c6e8a6f57cac7e027e4bfd2ff6a8a7673712ba6f
Vadik Sirekanyan <me@sirekanyan.org>
2023-09-09 19:03:12 +0400

    Updated jvm toolchain to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,12 +52,18 @@ publishing {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks {
     compileKotlin {
         kotlinOptions {
+            jvmTarget = "11"
             allWarningsAsErrors = true
         }
     }
